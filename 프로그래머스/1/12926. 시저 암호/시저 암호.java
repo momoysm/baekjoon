b186c79510ecf42765225276263e3a7ea0a59f6b
momoysm <90163578+momoysm@users.noreply.github.com>
2024-06-13 10:01:01 +0900

    [level 1] Title: 시저 암호, Time: 37.56 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -7,9 +7,9 @@ class Solution {
         for(int i=0; i<s.length(); i++) {
             char ch = s.charAt(i);
 
-            if(Character.isLowerCase(ch)) { //소문자
+            if(Character.isLowerCase(ch)) {
                 ch = (char) ((ch - 'a' + n) % 26 + 'a');
-            } else if(Character.isUpperCase(ch)) { //대문자
+            } else if(Character.isUpperCase(ch)) {
                 ch = (char) ((ch - 'A' + n) % 26 + 'A');
             }
 
