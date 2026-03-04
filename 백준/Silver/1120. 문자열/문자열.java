039ca89473f964b25eff386b0e4d48d8e94b3be7
SeongMo Yoon <90163578+momoysm@users.noreply.github.com>
2026-03-04 17:36:23 +0900

    [Silver IV] Title: 문자열, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.java
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -2,22 +2,20 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            String a = st.nextToken();
-            String b = st.nextToken();
+            String A = st.nextToken();
+            String B = st.nextToken();
 
-            int diff = b.length() - a.length(); // 길이 차이
+            int diff = B.length() - A.length();
             int result = Integer.MAX_VALUE;
 
-            // A를 0칸 ~ diff칸 뒤로 밀면서 모든 경우 체크
             for (int i = 0; i <= diff; i++) {
-                // B의 i번째부터 a.length()만큼 잘라서 비교
-                String b_sub = b.substring(i, i + a.length());
-                result = Math.min(result, getDiff(a, b_sub));
+                result = Math.min(result, getDiff(A, B.substring(i, i + A.length())));
             }
 
             bw.write(result + "");
@@ -25,18 +23,15 @@ public class Main {
         }
     }
 
-    private static int getDiff(String a, String b) {
-        int count = 0;
+    private static int getDiff(String A, String B) {
+        int result = 0;
 
-        char[] a_arr = a.toCharArray();
-        char[] b_arr = b.toCharArray();
+        char[] A_arr = A.toCharArray();
+        char[] B_arr = B.toCharArray();
 
-        for (int i = 0; i < a_arr.length; i++) {
-            if(a_arr[i] == b_arr[i]) continue;
-
-            count++;
+        for (int i = 0; i < A.length(); i++) {
+            if(A_arr[i] != B_arr[i]) result ++;
         }
-
-        return count;
+        return result;
     }
 }
