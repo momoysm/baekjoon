6f4402fb5100b2e312e4813a6391fcb72fa41fdd
SeongMo Yoon <90163578+momoysm@users.noreply.github.com>
2026-03-06 10:11:10 +0900

    [Silver I] Title: 팔, Time: 104 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.java
+++ 백준/Silver/1105. 팔/팔.java
@@ -2,27 +2,35 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
             String L = st.nextToken();
             String R = st.nextToken();
+
+            int result = 0;
 
             if(L.length() != R.length()) {
                 bw.write("0");
                 bw.flush();
                 return;
             }
-            
-            int count = 0;
+
             for (int i = 0; i < L.length(); i++) {
-                if(L.charAt(i) != R.charAt(i)) break; //앞자리가 다를 경우 ex) 800 / 900 이면 900때문에 0
-                if(L.charAt(i) == '8') count++; // 앞자리가 같을 경우 ex) 8xx / 8xx 이면 800때문에 최소 1
+                if(L.charAt(i) == R.charAt(i)) {
+                    if(L.charAt(i) == '8') {
+                        result++;
+                    }
+                } else {
+                    break;
+                }
             }
 
-            bw.write(count + "");
+            bw.write(result + "");
             bw.flush();
         }
     }
