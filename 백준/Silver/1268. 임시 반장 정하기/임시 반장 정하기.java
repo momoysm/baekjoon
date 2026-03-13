9e537b205b0c720d23963d9d2617d9d957e6ecc1
SeongMo Yoon <90163578+momoysm@users.noreply.github.com>
2026-03-13 16:14:46 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 160 ms, Memory: 15276 KB -BaekjoonHub

--- 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.java
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.java
@@ -11,38 +11,35 @@ public class Main {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-            int N = Integer.parseInt(br.readLine());
-            int[][] classInfo = new int[N+1][6];
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] classInfo = new int[n + 1][6];
 
-            for (int i = 1; i <= N; i++) {
+            for (int i = 1; i <= n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-                for (int grade = 1; grade <= 5; grade++) {
+                for (int grade = 1; grade < 6; grade++) {
                     classInfo[i][grade] = Integer.parseInt(st.nextToken());
                 }
             }
 
+            int max = -1;
             int result = 1;
-            int maxCount = 0;
 
-            for (int i = 1; i <= N; i++) {
+            for (int i = 1; i <= n; i++) { // 기준 학생
                 int count = 0;
-                for (int j = 1; j <= N; j++) {
+
+                for (int j = 1; j <= n; j++) { // 비교 학생
                     if(i == j) continue;
 
-                    boolean sameClass = false;
-                    for (int grade = 1; grade <= 5; grade++) {
+                    for (int grade = 1; grade < 6; grade++) {
                         if(classInfo[i][grade] == classInfo[j][grade]) {
-                            sameClass = true;
+                            count++;
                             break;
                         }
                     }
-                    if(sameClass) {
-                        count++;
-                    }
                 }
 
-                if(maxCount < count) {
-                    maxCount = count;
+                if(max < count) {
+                    max = count;
                     result = i;
                 }
             }
